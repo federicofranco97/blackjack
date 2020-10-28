c444dfc6e3f6a4aca4e17483b3ff511ddd0195b1
FedeFranco97 <federicoafranco97@gmail.com>
2020-10-28 01:30:25 -0300

    modifs interfac

--- Cliente.py
+++ Cliente.py
@@ -1,14 +1,32 @@
 import os
 import random
+import select
 import socket
+import sys
 
 from pip._vendor.distlib.compat import raw_input
 
+hayJugadoresEnEspera = False
+hayPartidaEnCurso = True
 
-# Metodo para conectarse con el servidor
-def conectar():
+
+def imprimirEstadoServidor():
+    if hayJugadoresEnEspera:
+        print("Hay una partida en curso actualmente, al finalizar la ronda podra solicitar unirse a la misma")
+    if not hayPartidaEnCurso and hayJugadoresEnEspera:
+        print("No hay una partida iniciada, pero hay jugadores en la cola, la partida comenzara brevemente.")
+    if not hayPartidaEnCurso and not hayJugadoresEnEspera:
+        print("No hay una  partida iniciada, y no hay jugadores en cola,"
+              " si no se conecta nadie en breve comenzara solo contra la banca")
+
+
+# Metodo de inicializacion, pide el nombre de usuario y realiza la conexion al sv
+def inicioCliente():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(200)
+    # Se conecta con el servidor
     # host = '192.168.100.233'
-    #port = 9999
+    # port = 9999
     host = raw_input("Por favor ingresa la IP del servidor: ")
     port = raw_input("Por favor ingresa el puerto del servidor: ")
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -16,15 +34,30 @@ def conectar():
         sock.connect((host, port))
     except Exception as e:
         print("Ocurrio un error al conectarse con el servidor:", e)
+        return
     print("Conectado, bienvenido al servidor!")
-
-
-#Consulta el estado del servidor (en partida/esperando jugadores)
-def consultarEstado():
-        
-
-
+    name = raw_input("Bienvenido al servidor de BlackJack, por favor ingrese su nombre: ")
+    # si se pudo conectar, envio el nombre del jugador
+    s.send(bytes(name, 'utf-8'))
+    imprimirEstadoServidor()
+    while 1:
+        socket_list = [sys.stdin, s]
+        # Obtenemos la lista de socket disponibles
+        rList, wList, error_list = select.select(socket_list, [], [])
+        for sock in rList:
+            # Mensaje entrante del servidor, si no hay datos, se informa la desconexion y se cierra el cliente
+            if sock == s:
+                data = sock.recv(4096)
+                if not data:
+                    print('No se pudo conectar con el servidor!!')
+                    sys.exit()
+                else:
+                    sys.stdout.write(data)
+            # El usuario envia un mensaje al servidor
+            else:
+                msg = sys.stdin.readline()
+                s.send(msg)
 
 
 if __name__ == "__main__":
-    conectar()
+    inicioCliente()
