bd7a5f50451c3803b19627bc114778ddecf91f3d
FedeFranco97 <federicoafranco97@gmail.com>
2020-10-29 19:05:52 -0300

    cambios cliente

--- Cliente.py
+++ Cliente.py
@@ -3,11 +3,26 @@ import random
 import select
 import socket
 import sys
+from _thread import *
+import threading
 
 from pip._vendor.distlib.compat import raw_input
 
 hayJugadoresEnEspera = False
 hayPartidaEnCurso = True
+
+
+def escucharServidor(sock):
+    while 1:
+        try:
+            data = sock.recv(4096)
+            if not data:
+                continue
+            else:
+                # Imprimo los msjs del servidor
+                print(str(data))
+        except socket.timeout:
+            print("Se perdio la conexion con el servidor")
 
 
 # Muestra el estado del servidor, si hay partidas en curso y si hay gente en espera
@@ -21,7 +36,12 @@ def imprimirEstadoServidor():
               " si no se conecta nadie en breve comenzara solo contra la banca")
 
 
-# Metodo de inicializacion, pide el nombre de usuario y realiza la conexion al sv
+"""
+    Metodo que inicializa el cliente, solicita los datos principales como ip del servidor,
+    puerto, y luego de realizar la conexion pide el nombre de usuario
+"""
+
+
 def inicioCliente():
     print("Bienvenido al servidor de BlackJack")
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -38,6 +58,7 @@ def inicioCliente():
         print("Ocurrio un error al conectarse con el servidor:", e)
         return
     print("Conectado, bienvenido al servidor!")
+
     # name = raw_input("Por favor ingrese su nombre: ")
     # si se pudo conectar, envio el nombre del jugador
     # s.send(bytes(name, 'utf-8'))
@@ -48,39 +69,14 @@ def inicioCliente():
         sys.exit()
     else:
         # Primera respuesta del servidor (Ingresa tu nombre)
-        print(str(data),":")
+        print(str(data), ":")
         name = raw_input()
         sock.send(name.encode())
 
-    sock.settimeout(200)
+    start_new_thread(escucharServidor(sock))
     while 1:
-        try:
-            data = sock.recv(4096)
-            if not data:
-                continue
-            else:
-                # Imprimo los msjs del servidor
-                print(str(data))
-        except socket.timeout:
-            print("Se perdio la conexion con el servidor")
-
-    # while 1:
-    #     socket_list = [socket.socket(), s]
-    #     # Obtenemos la lista de socket disponibles
-    #     rList, wList, error_list = select.select(socket_list, [], [])
-    #     for sock in rList:
-    #         # Mensaje entrante del servidor, si no hay datos, se informa la desconexion y se cierra el cliente
-    #         if sock == s:
-    #             data = sock.recv(4096)
-    #             if not data:
-    #                 print('No se pudo conectar con el servidor!!')
-    #                 sys.exit()
-    #             else:
-    #                 sys.stdout.write(data)
-    #         # El usuario envia un mensaje al servidor
-    #         else:
-    #             msg = sys.stdin.readline()
-    #             s.send(msg)
+        newMsg = sys.stdin.readline()
+        s.send(newMsg.encode())
 
 
 if __name__ == "__main__":
